b617508fb46a0ec04290bb7723da04a7e8c0e75c
Saicharan Challapalli <saicharan.challapalli10@gmail.com>
2026-03-28 18:33:27 -0400

    Fixed bugs in ParkingSpot class

--- src/ParkingSpot.cpp
+++ src/ParkingSpot.cpp
@@ -8,35 +8,41 @@ ParkingSpot::ParkingSpot(int number) {
     assignedTo = "Assigned to no one";
     carModel = "No car";
 }
+
 //gets parking spot number for a user
 int ParkingSpot::getSpotNumber() const {
     return spotNumber;
 }
+
 //checks if parking spot is taken
 bool ParkingSpot::getIsAvailable() const {
     return isAvailable;
 }
-//Sees who the parking spot is asinged to 
+
+//Sees who the parking spot is asinged to
 string ParkingSpot::getAssignedTo() const {
     return assignedTo;
 }
+
 //gets car model associated with parking spot
 string ParkingSpot::getCarModel() const {
     return carModel;
 }
+
 // THis was very diffuclt oto program as a function. So applgies if it is janky or done wrong! Assigns parking spot to a user object
-void ParkingSpot::reserveSpot(const string& userName, const string& userCarModel) {
+void ParkingSpot::reserveSpot(const string& userName, const string& userCarModel, int takenSpots) {
     if (isAvailable) {
         isAvailable = false;
         assignedTo = userName;
         carModel = userCarModel;
         cout << "Spot " << spotNumber << " successfully reserved for " << userName << endl;
         cout << "Car Model: " << carModel << endl;
-        cout << "Total Fee: $" << pricing.calculateFee() << endl;
+        cout << "Total Fee: $" << pricing.calculateFee(takenSpots) << endl;
     } else {
         cout << "Spot " << spotNumber << " is already taken!" << endl;
     }
 }
+
 //Release parking spot for a user
 void ParkingSpot::releaseSpot() {
     isAvailable = true;
@@ -44,7 +50,8 @@ void ParkingSpot::releaseSpot() {
     carModel = "No car!";
     cout << "Spot " << spotNumber << " is now available." << endl;
 }
-//checks to see if parking spot is avliable 
+
+//checks to see if parking spot is avliable
 void ParkingSpot::checkSpot() const {
     if (isAvailable) {
         cout << "Spot " << spotNumber << " is available." << endl;
@@ -52,7 +59,8 @@ void ParkingSpot::checkSpot() const {
         cout << "Spot " << spotNumber << " is taken by " << assignedTo << " - " << carModel << endl;
     }
 }
-//This allows a log to be printed on the website to see who has which parking spot 
+
+//This allows a log to be printed on the website to see who has which parking spot
 string ParkingSpot::getStatusInfo() const {
     if (isAvailable) {
         return "Spot " + to_string(spotNumber) + " ################ Assigned to no one! ###################No car!";
--- src/ParkingSpot.h
+++ src/ParkingSpot.h
@@ -5,26 +5,26 @@
 #include "PricingEngine.h"
 using namespace std;
 
-//class decleration
 class ParkingSpot {
-//Private data types
 private:
     int spotNumber;
     bool isAvailable;
     string assignedTo;
     string carModel;
     PricingEngine pricing;
-//Public data types
+
 public:
     ParkingSpot(int number);
+
     int getSpotNumber() const;
     bool getIsAvailable() const;
     string getAssignedTo() const;
     string getCarModel() const;
-    void reserveSpot(const string& userName, const string& userCarModel);
+
+    void reserveSpot(const string& userName, const string& userCarModel, int takenSpots);
     void releaseSpot();
     void checkSpot() const;
     string getStatusInfo() const;
 };
 
-#endif 
+#endif // PARKINGSPOT_H
