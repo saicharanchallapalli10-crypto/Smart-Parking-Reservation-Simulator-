42e1b2af965caeef6150b1f7470c7d1f540ad247
sirrizzington67 <zielinskI.a@northeastern.edu>
2026-04-01 02:47:04 -0400

    Fixed a bug in Pricing Engine cpp regarding hours and continued writing the README

---
+++ README.md
@@ -0,0 +1,3 @@
+Smart Parking Reservation Simulator
+Sitarama Challapalli Adrian Zielinski John Nevin
+This project is supposed to create a smart parking reservation system so as more parking spots are reserved and occupied, the price for the remaining spots increase.
--- src/PricingEngine.cpp
+++ src/PricingEngine.cpp
@@ -12,7 +12,11 @@ double PricingEngine::getBaseRate() const {
     return baseRate;
 }
 
+<<<<<<< HEAD
 // returns the updated baserate that increases by 30 cents for every parking spot taken, multiplied by hours
+=======
+// returns the updated baserate that increases by 10 cents for every parking spot taken
+>>>>>>> fbc88af (Fixed a bug in Pricing Engine cpp regarding hours and continued writing the README)
 double PricingEngine::calculateFee(int takenSpots, int hours) const {
     return (baseRate + (takenSpots * 0.30)) * hours;
 }
